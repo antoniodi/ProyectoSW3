f46bd1f5b6273b33991c8bcd56cfc55b75fad4f0
sgomezmeyer <meyersailer@gmail.com>
2016-11-03 22:20:45 -0500

    clase Obra

---
+++ Obra.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Obra here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Obra
+{
+    // instance variables - replace the example below with your own
+    private String habilidad;
+    private String elemento;
+
+    /**
+     * Constructor for objects of class Obra
+     */
+    public Obra(String habilidad, String elemento)
+    {
+        // initialise instance variables
+        this.habilidad=habilidad;
+        this.elemento=elemento;
+    }
+
+}
