1ceb3ded038baee6ccd1ad08a792d3668a1352f3
antoniodi <antonio4793@hotmail.com>
2016-11-03 19:47:01 -0500

    Agregada la clase Calificacion

---
+++ Calificacion.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Calificacion here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Calificacion
+{
+    // instance variables - replace the example below with your own
+    private String comentario;
+    private int valoracion;
+
+    /**
+     * Constructor for objects of class Calificacion
+     */
+    public Calificacion(string comentario, int calificacion)
+    {
+        // initialise instance variables
+       this.comentario= comentario;
+       this.valoracion= valoracionn;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod()
+    {
+        // put your code here
+        return 0;
+    }
+    
+}
